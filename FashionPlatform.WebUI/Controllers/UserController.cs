884c2d49e10741b909906a799cc2015252507966
iurie josan <josan.iurie@gmail.com>
2018-03-03 00:23:43 +0200

    instal references

---
+++ FashionPlatform.WebUI/Controllers/UserController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FashionPlatform.WebUI.Controllers
+{
+    public class UserController : Controller
+    {
+        // GET: User
+        public ActionResult Index()
+        {
+            Dictionary<string, object> data
+                = new Dictionary<string, object>();
+            data.Add("Ключ", "Значение");
+
+            return View(data);
+        }
+    }
+}
