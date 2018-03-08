545a871b469b98e67baa3012b709d9cf85572e8e
iurie josan <josan.iurie@gmail.com>
2018-03-08 14:26:23 +0200

    Add properties of product

--- FashionPlatform.Domain/Entities/Product.cs
+++ FashionPlatform.Domain/Entities/Product.cs
@@ -25,10 +25,182 @@ namespace FashionPlatform.Domain.Entities
         public string Category { get; set; }
 
         [Required]
-        [Range(0.01,Double.MaxValue,ErrorMessage = "Please enter positive value")]
+        [Range(0.01, Double.MaxValue, ErrorMessage = "Please enter positive value")]
         public decimal Price { get; set; }
 
         public byte[] ImageData { get; set; }
         public string ImageMimeType { get; set; }
+        public string Country { get; set; }
+        public Shoes ShoesEnum { get; set; }
+        public Up UpWearEnum { get; set; }
+        public Down DownWearEnum { get; set; }
+        public Accesories AccesoriesEnum { get; set; }
+        public ProductDestinasion ProductDestinasionEnum { get; set; }
+        public TypeOfFiber TypeOfFiber { get; set; }
+        public Cloth Cloth { get; set; }
+        public Style StyleEnum { get; set; }
+        public DressCode DressCodeEnum { get; set; }
+
     }
+
+    public enum Shoes
+    {
+        Balletshoes,        //балетки
+        HighBoots,          //сапоги
+        HighHeelShoes,  // туфли
+        LowBoots        //ботинки
+    }
+
+    public enum Up
+    {
+        Shirt,      //Рубашка
+        Undershirt, //Майка
+        Blouze,     //блузка
+        TShirt,     //футболка
+        Cardigan,   //Кардиган
+        Jacket,     //пиджак
+        Dress,      //платье
+        Overalls,   //комбинезон
+        Pullover,   //свитер
+        Bolero,     //болеро
+    }
+
+    public enum Down
+    {
+        Overalls,   //комбинезон
+        Dress,      //платье
+        Pants,      //брюки
+        Capris,     //капри
+        Skirt,      //юбка
+        Tights,     //колготы
+        Soks,       //носки
+        Stockings   //чулки
+    }
+
+    public enum Accesories
+    {
+        Rings,      //кольца
+        Eerrings,   //серьги
+        Necklaces,  //ожерелье
+        Braclets,   //браслет
+        Bags,       //сумка
+        Strap,      //ремень
+        Shawl,      //платок
+        Scarf,      //шарф
+        Kerchief,   //косынка
+
+    }
+
+    public enum ProductDestinasion
+    {
+        EveningWear,    //вечерние наряды
+        Blouse,         //блузка
+        Underwear,      //нижнее белье
+        SummerWear,     //летняя одежда
+        WinterWear,     //зимняя одежда
+        EveryDayWear,   //повседневная одежда
+        Accessories,    //аксессуары
+        BusinessWear,   //деловая одежда
+        BaseWear,       //базовая одежда
+        SportsWear      //спортивная одежда
+    }
+
+
+    public class TypeOfFiber        //Тип волокна
+    {
+        public enum Natural
+        {
+            Cotton,     //хлопок
+            Flax,       //лен
+            Asbestos,   //асбест
+            Wool,       //шерсть
+            Silk        //шелк
+        }
+
+        public enum Synthetic
+        {
+            Viscose,    //вискоза
+            Acetate,    //ацетат
+            Polyester,  //полиэстер
+            Acrylic,    //акрил
+            Nylon,      //нейлон
+            Polyurethane//полиуретан 
+        }
+
+    }
+
+    public class Cloth      //Ткань
+    {
+        public enum Thin        //Тонкая
+        {
+            Batiste,    //батист
+            Marquisite, //маркизет
+            Voile,      //вуаль
+            Chiffon,    //шифон
+            Georgette,  //жоржет
+        }
+
+        public enum MediumThickness     //Средней толщины
+        {
+            Cashmere,   //кашемир
+            Knitwear,   //трикотаж
+        }
+
+        public enum Cloak   //Плащевые
+        {
+            Atlas,      //атлас
+            Satin,      //Сатин
+            Taffeta,    //тафта
+            Brocade,    //Парча
+            Damascus,   //дамаск
+            Velvet,     //бархат
+            Velveteen,  //вельвет
+            Velours,    //велюр
+            Bike,       //байка
+            CrepeВeСhine,//крепдешин
+            Organza,    //органза
+            }
+
+        public enum Thick
+        {
+            Gabardine,  //габардин
+            Cloth,      //сукно
+            DiagonalCloth,//диагональ
+            Tweed,      //твид
+            Jersey,     //джерси
+            Boucle,     //букле
+            Drapery,    //драп
+            Ratin,      //ратин
+            Leather,    //кожа
+            Fur,        //мех
+            PileCloth, //ткань с ворсом
+        }
+    }
+
+    public enum Style
+    {
+        Official,       //официальный
+        AvantGarde,     //авангард
+        Glamor,         //гламур
+        Classic,        //классика
+        Country,        //кантри
+        Bohemia,        //богема
+        Vintage,        //винтаж
+        Grunge,         //гранж
+        Casual,         //кэжуал
+        Military,       //милитари
+        }
+
+    public enum DressCode
+    {
+        Street,         //уличный
+        Everyday,       //повседневный
+        DayToDayBusiness,//повседневно-деловой
+        DayToDayElegantly,//элегантно-повседневный
+        BusinessStyle,  //деловой стиль
+        SpecialBusinessStyle,//деловой стиль для особых случаев
+
+
+    }
+
 }
